56e2814a0ece39d10bee208b2c37821c716c662d
msaaddev <mrsaadirfan@gmail.com>
2021-04-14 19:57:53 +0500

    📦 NEW: full screen

--- src/script.js
+++ src/script.js
@@ -1,28 +1,28 @@
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // cursor
 const cursor = {
-    x: 0,
-    y: 0,
+	x: 0,
+	y: 0,
 };
 
-window.addEventListener('mousemove', e => {
-    cursor.x = e.clientX / sizes.width - 0.5;
-    cursor.y = -(e.clientY / sizes.height - 0.5);
+window.addEventListener("mousemove", (e) => {
+	cursor.x = e.clientX / sizes.width - 0.5;
+	cursor.y = -(e.clientY / sizes.height - 0.5);
 });
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl');
+const canvas = document.querySelector("canvas.webgl");
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600,
+	width: window.innerWidth,
+	height: window.innerHeight,
 };
 
 // Scene
@@ -30,13 +30,18 @@ const scene = new THREE.Scene();
 
 // Object
 const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+	new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+	new THREE.MeshBasicMaterial({ color: 0xff0000 })
 );
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100
+);
 
 // const aspectRatio = sizes.width / sizes.height;
 // const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100);
@@ -53,7 +58,7 @@ controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
+	canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 
@@ -61,25 +66,25 @@ renderer.setSize(sizes.width, sizes.height);
 const clock = new THREE.Clock();
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+	const elapsedTime = clock.getElapsedTime();
 
-    // Update objects
-    // mesh.rotation.y = elapsedTime;
+	// Update objects
+	// mesh.rotation.y = elapsedTime;
 
-    // update camera
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-    // camera.position.y = cursor.y * 5;
-    // camera.lookAt(mesh.position);
+	// update camera
+	// camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+	// camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+	// camera.position.y = cursor.y * 5;
+	// camera.lookAt(mesh.position);
 
-    // update controls for damping
-    controls.update();
+	// update controls for damping
+	controls.update();
 
-    // Render
-    renderer.render(scene, camera);
+	// Render
+	renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick);
 };
 
 tick();
